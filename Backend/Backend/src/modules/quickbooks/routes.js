30d45880dfcaebfdc08e12af55c785eab79c41fb
pvasavi123 <vvasavipaishetti@gmail.com>
2026-07-30 17:27:51 +0530

    jwt routes

--- Backend/Backend/src/modules/quickbooks/routes.js
+++ Backend/Backend/src/modules/quickbooks/routes.js
@@ -4,20 +4,21 @@ const express    = require('express');
 const router     = express.Router();
 const controller = require('./controller');
 const { validateQuickBooksState } = require('../../core/middleware/oauthMiddleware');
+const { authenticate } = require('../auth/auth.middleware');
 
 // OAuth
 router.get('/connect',    controller.connectQuickbooks);
 router.get('/callback',   validateQuickBooksState, controller.quickbooksCallback);
-router.post('/disconnect', controller.disconnectQuickbooks);
-router.get('/tokens/',    controller.listQuickbooksTokens);
+router.post('/disconnect', authenticate, controller.disconnectQuickbooks);
+router.get('/tokens/',    authenticate, controller.listQuickbooksTokens);
 
 // Data endpoints
-router.get(['/customers', '/customers/'], controller.getCustomers);
-router.get(['/vendors', '/vendors/'],     controller.getVendors);
-router.get(['/accounts', '/accounts/'],   controller.getAccounts);
-router.get(['/classes', '/classes/'],     controller.getClasses);
-router.get(['/locations', '/locations/'], controller.getLocations);
-router.get(['/company', '/company/'],     controller.getCompanyInfo);
-router.get(['/export', '/export/'],       controller.exportMasterData);
+router.get(['/customers', '/customers/'], authenticate, controller.getCustomers);
+router.get(['/vendors', '/vendors/'],     authenticate, controller.getVendors);
+router.get(['/accounts', '/accounts/'],   authenticate, controller.getAccounts);
+router.get(['/classes', '/classes/'],     authenticate, controller.getClasses);
+router.get(['/locations', '/locations/'], authenticate, controller.getLocations);
+router.get(['/company', '/company/'],     authenticate, controller.getCompanyInfo);
+router.get(['/export', '/export/'],       authenticate, controller.exportMasterData);
 
 module.exports = router;
--- Backend/Backend/src/modules/xero/routes.js
+++ Backend/Backend/src/modules/xero/routes.js
@@ -4,19 +4,20 @@ const express    = require('express');
 const router     = express.Router();
 const controller = require('./controller');
 const { validateXeroState } = require('../../core/middleware/oauthMiddleware');
+const { authenticate } = require('../auth/auth.middleware');
 
 // OAuth
 router.get('/connect',             controller.connectXero);
 router.get('/callback',            validateXeroState, controller.xeroCallback);
-router.post('/select-companies',   controller.selectCompanies);
-router.post('/disconnect',         controller.disconnectXero);
-router.get(['/tokens', '/tokens/'], controller.listXeroTokens);
+router.post('/select-companies',   authenticate, controller.selectCompanies);
+router.post('/disconnect',         authenticate, controller.disconnectXero);
+router.get(['/tokens', '/tokens/'], authenticate, controller.listXeroTokens);
 
 // Data endpoints
-router.get(['/contacts', '/contacts/'],         controller.getContacts);
-router.get(['/accounts', '/accounts/'],         controller.getAccounts);
-router.get(['/classes', '/classes/'],           controller.getClasses);
-router.get(['/locations', '/locations/'],       controller.getLocations);
-router.get(['/organisation', '/organisation/'], controller.getOrganisation);
+router.get(['/contacts', '/contacts/'],         authenticate, controller.getContacts);
+router.get(['/accounts', '/accounts/'],         authenticate, controller.getAccounts);
+router.get(['/classes', '/classes/'],           authenticate, controller.getClasses);
+router.get(['/locations', '/locations/'],       authenticate, controller.getLocations);
+router.get(['/organisation', '/organisation/'], authenticate, controller.getOrganisation);
 
 module.exports = router;
